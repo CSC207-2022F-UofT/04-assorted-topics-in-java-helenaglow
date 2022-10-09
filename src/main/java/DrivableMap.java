4cb951e003a898b0a9633aafe2c5b816f3c56f96
Helena Glowacki <helena.glowacki@mail.utoronto.ca>
2022-10-08 19:28:11 -0500

    Finished all the ToDo assignments in DrivableMap, DrivableTrader, and Trader.

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -7,9 +7,7 @@
  * created the constructor for you already.
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 class DrivableMap {
     HashMap<String, Drivable> drivable_map;
@@ -27,9 +25,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
-
+    public Boolean addDrivable(String id, Drivable item){
+        if (drivable_map.containsKey(id)) {
+            return false;
+        } else {
+            drivable_map.put(id, item);
+            return true;
+        }
+    }
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
@@ -37,17 +40,28 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
+    public Boolean hasFasterThan(int speed){
+        for(Drivable i : drivable_map.values()){
+            if (i.getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable(){
+        List<Tradable> tradableItems = new ArrayList<Tradable>();
+        for(Drivable i : drivable_map.values()){
+            if (i instanceof Tradable){
+                tradableItems.add((Tradable) i);
+            }
+        }
+        return tradableItems;
+    }
 
-
-
-    
 }
